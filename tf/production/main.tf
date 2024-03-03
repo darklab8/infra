d95b5421f9f42b4312f242939032ad6fd12eb901
dd84ai <dd84ai@gmail.com>
2024-03-03 16:13:31 +0100

    refactor: code to be more neat

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -2,27 +2,8 @@ module "ssh_key" {
    source       = "../modules/hetzner_ssh_key"
 }
 
-module "cluster" {
-  source       = "../modules/hetzner_server"
-  environment  = "production"
-  name         = "node-arm"
-  server_power = "cax31"
-  backups      = false
-  ssh_key_id   = module.ssh_key.id
-  datacenter   = "hel1-dc2"
-}
-
-module "node_arm" {
-   source       = "../modules/ansible_microk8s"
-   hostname     = module.cluster.cluster_ip
-}
-
 module "labels" {
    source       = "../modules/kube_node_labels"
-}
-
-output "cluster_ip" {
-  value = module.cluster.cluster_ip
 }
 
 data "aws_ssm_parameter" "hetzner" {
@@ -32,9 +13,3 @@ data "aws_ssm_parameter" "hetzner" {
 locals {
   secrets = nonsensitive(jsondecode(data.aws_ssm_parameter.hetzner.value))
 }
-
-provider "docker" {
-  host     = "ssh://root@${module.stack.cluster_ip}:22"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "~/.ssh/id_rsa.darklab"]
-}
-
