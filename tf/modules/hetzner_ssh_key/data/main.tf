fcafa1a904fa5244809e359793252db08e04c09c
dd84ai <dd84ai@gmail.com>
2024-03-03 01:20:22 +0100

    feat: access to ssh key

--- tf/modules/hetzner_ssh_key/data/main.tf
+++ tf/modules/hetzner_ssh_key/data/main.tf
@@ -1,3 +1,11 @@
+data "hcloud_ssh_key" "darklab" {
+  name       = "darklab_key"
+}
+
+output id {
+  value = data.hcloud_ssh_key.darklab.id
+}
+
 terraform {
   required_providers {
     hcloud = {
--- tf/modules/hetzner_ssh_key/main.tf
+++ tf/modules/hetzner_ssh_key/main.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = ">= 1.35.2"
+      version = ">= 1.45.0"
     }
   }
 }
