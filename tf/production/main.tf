0817f23fbd02df1655758c0cecccdc0935b99427
dd84ai <dd84ai@gmail.com>
2024-03-17 21:50:14 +0100

    refactor: monitoring deployment to be without tars

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -7,7 +7,7 @@ module "cluster" {
 }
 
 module "argo" {
-  source  = "github.com/darklab8/argocd-cue.git?ref=v0.2.0"
+  source  = "github.com/darklab8/argocd-cue.git?ref=v0.2.4"
   context = "darklab"
 }
 
