2d8047cfc35b4035440c5077849b1558ed9bc97d
dd84ai <dd84ai@gmail.com>
2024-06-09 14:02:41 +0200

    refactor: recreated cluster. keeping some components down

--- tf/production/main.tf
+++ tf/production/main.tf
@@ -6,15 +6,21 @@ module "cluster" {
   source = "../modules/kube_cluster"
 }
 
-module "argo" {
-  source  = "github.com/darklab8/argocd-cue.git?ref=v0.4.0-a3"
-  context = "darklab"
-}
+# module "argo" {
+#   source  = "github.com/darklab8/argocd-cue.git?ref=v0.4.0-a3"
+#   context = "darklab"
+#   depends_on = [
+#     module.cluster,
+#   ]
+# }
 
-module "discovery" {
-  source      = "../modules/kube_argo_discovery"
-  environment = "production"
-  project     = "infra"
-  repo_url    = "https://github.com/darklab8/infra.git"
-  repo_path   = "k8s/production"
-}
+# module "discovery" {
+#   source      = "../modules/kube_argo_discovery"
+#   environment = "production"
+#   project     = "infra"
+#   repo_url    = "https://github.com/darklab8/infra.git"
+#   repo_path   = "k8s/production"
+#   depends_on = [
+#     module.argo,
+#   ]
+# }
