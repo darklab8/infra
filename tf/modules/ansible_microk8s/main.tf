e8f1613e2e91a8bc6f398cd5f4a7d4077b681bb2
dd84ai <dd84ai@gmail.com>
2025-01-19 12:34:11 +0100

    feat: bring microk8s again online

--- tf/modules/ansible_microk8s/main.tf
+++ tf/modules/ansible_microk8s/main.tf
@@ -6,16 +6,23 @@ terraform {
   }
 }
 
+variable "kube_version" {
+  description = "Version of microk8s, for example 1.31" # IMPORTANT WITHOUT `v`
+  type        = string
+}
+
 resource "ansible_playbook" "playbook" {
   playbook   = "${path.module}/deploy.yml"
   name       = var.hostname
   replayable = false
+  verbosity  = 2
 
   extra_vars = {
     ansible_user : "root"
     ansible_connection : "ssh"
     # ansible_ssh_extra_args: "-o ForwardAgent=yes"
     ansible_ssh_private_key_file : "~/.ssh/id_rsa.darklab"
+    kube_version = var.kube_version
   }
 }
 
--- tf/production/kube.tf
+++ tf/production/kube.tf
@@ -1,7 +1,8 @@
-# module "microk8s" {
-#   source   = "../modules/ansible_microk8s"
-#   hostname = module.node_darklab.ipv4_address
-# }
+module "microk8s" {
+  source       = "../modules/ansible_microk8s"
+  hostname     = module.node_darklab.ipv4_address
+  kube_version = "1.31"
+}
 
 # module "argo" {
 #   source  = "github.com/darklab8/argocd-cue.git?ref=v0.4.0-a3"
