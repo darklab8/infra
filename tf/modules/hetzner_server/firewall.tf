a609eab2a8869cfdaecb3e7b345c88809ca48a07
dd84ai <dd84ai@gmail.com>
2025-02-13 21:28:32 +0100

    ops: add a bit of firewall

---
+++ tf/modules/hetzner_server/firewall.tf
@@ -0,0 +1,67 @@
+resource "hcloud_firewall" "basic_firewall" {
+  name = "firewall-${var.name}"
+
+  // ping
+  rule {
+    direction = "in"
+    protocol  = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "udp"
+    port      = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "udp"
+    port      = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_firewall_attachment" "fw_ref" {
+  firewall_id = hcloud_firewall.basic_firewall.id
+  server_ids  = [hcloud_server.cluster.id]
+}
